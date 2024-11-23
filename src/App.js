29d8a941ce46966ff7922c435969cbd05a50baa9
MogiliManikanta <you@example.com>
2024-11-24 00:01:34 +0530

    23/11/24

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navigation from "./customer/components/navigation/Navigation";
+import HomePage from "./customer/pages/HomePage/HomePage";
+import Footer from "./customer/components/Footer/Footer";
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <div>
+        <HomePage />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+// import "react-alice-carousel/lib/alice-carousel.css";
+import "react-alice-carousel/lib/alice-carousel.css";
+
+import { MainCarouselData } from "./MainCarouselData";
+
+// const responsive = {
+//   0: { items: 1 },
+//   568: { items: 2 },
+//   1024: { items: 3 },
+// };
+
+const MainCarousel = () => {
+  const items = MainCarouselData.map((item) => (
+    <img
+      role="presentation"
+      className="cursor-pointer"
+      src={item.image}
+      alt={item.path}
+    />
+  ));
+  return (
+    <AliceCarousel
+      items={items}
+      disableButtonsControls
+      //   disableDotsControls
+      autoPlay
+      autoPlayInterval={1000}
+      infinite
+    />
+  );
+};
+
+export default MainCarousel;
