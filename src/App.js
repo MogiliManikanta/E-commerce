677f1cd5e1de5f0134c8d253ee7e3a68f20174a9
MogiliManikanta <you@example.com>
2024-11-24 23:11:01 +0530

    24/11/24

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Navigation from "./customer/components/navigation/Navigation";
 import HomePage from "./customer/pages/HomePage/HomePage";
 import Footer from "./customer/components/Footer/Footer";
 import Product from "./customer/components/product/Product";
+import ProductDetails from "./customer/components/ProductDetails/ProductDetails";
 function App() {
   return (
     <div>
       <Navigation />
       <div>
-        {/* <HomePage />*/}
-        <Product />
+        {/* <HomePage />
+        <Product />*/}
+        <ProductDetails />
       </div>
       <Footer />
     </div>
